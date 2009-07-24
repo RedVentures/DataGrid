b0e059f2de1492583163f8e7c2ca8565bc861099
Adam Wagner <awagner@redventures.com>
2009-07-24 00:09:14 -0400

    Setup test runner for unittests and added to Makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 test:
-	@python -m doctest datagrid/*.py && echo "All tests passed"
+	@python -m doctest datagrid/*.py && echo "All doc-tests passed"
+	@echo '----------------------------------------------------------------------'
+	@python tests/runner.py
 
 testverbose:
-	@python -m doctest datagrid/*.py -v && echo "All tests passed"
+	python -m doctest datagrid/*.py -v && echo "All doc-tests passed"
+	@echo '----------------------------------------------------------------------'
+	python tests/runner.py
 
 clean:
 	@find . -name '*.pyc' | xargs rm -f
---
+++ tests/runner.py
@@ -0,0 +1,31 @@
+#------------------------------------------------------------------------#
+# DataGrid - Tabular Data Rendering Library
+# Copyright (C) 2009 Adam Wagner <awagner@redventures.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published 
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------#
+
+import unittest
+import core, calctools
+
+# Create test suite
+suite = unittest.TestSuite()
+
+# Attach all appropriate test-modules
+for module in [core, calctools]:
+    suite.addTest(unittest.TestLoader().loadTestsFromModule(module))
+
+# Begin tests
+unittest.TextTestRunner(verbosity=2).run(suite)
+
