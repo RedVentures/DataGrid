bbcbd159a30ac1d2f2501d1a4dfe0f9e24da7bfc
Adam Wagner <awagner@redventures.com>
2009-08-08 15:45:44 -0400

    Dummy renderer to be used in future testing

---
+++ tests/mocks/dummyrenderer.py
@@ -0,0 +1,35 @@
+#------------------------------------------------------------------------#
+# DataGrid - Tabular Data Rendering Library
+# Copyright (C) 2009 Adam Wagner <awagner@redventures.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published 
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------#
+
+"""Dummy Renderer Module"""
+
+def table(config, head, body, tail):
+    return ""
+
+def row(config, cells, level=0, name=None, value=None):
+    return ""
+
+def cell(config, data, column): 
+    return ""
+
+def head(config):
+    return ""
+
+def tail(config, cells):
+    return ""
+
